7a708ae2efb6648e42942d83cb83b428816b1e69
TGA-Student <tga.student@hackreactor.com>
2015-07-24 16:48:11 -0700

    created table

--- server/db/index.js
+++ server/db/index.js
@@ -5,3 +5,10 @@ var mysql = require('mysql');
 // and to the database "chat".
 
 
+var dbConnection = mysql.createConnection({
+	user: "root",
+	password: "vyjamil",
+	database: "chat"
+});
+
+dbConnection.connect();
--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,57 @@
-CREATE DATABASE chat;
+
 
 USE chat;
 
-CREATE TABLE messages (
+-- CREATE TABLE messages (
   /* Describe your table here.*/
+
+
+
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+-- 
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+		
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(30) NULL DEFAULT NULL,
+  `message` VARCHAR(200) NULL DEFAULT NULL,
+  `room` VARCHAR(20) NULL DEFAULT NULL,
+  `messageID` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+KEY (`id`)
 );
+
+-- ---
+-- Foreign Keys 
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`username`,`message`,`room`,`messageID`) VALUES
+-- ('','','','','');
+
+
+-- );
 
 /* Create other tables and define schemas for them here! */
 
